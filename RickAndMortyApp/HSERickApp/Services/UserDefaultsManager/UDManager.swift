3452c38c1a84a50e81814459687a8908136023a4
Koryakin Alexey <~>
2022-05-14 13:21:41 +0300

    UD Теперь хранит полную модель персонажа вместо ID

--- RickAndMortyApp/HSERickApp/Services/UserDefaultsManager/UDManager.swift
+++ RickAndMortyApp/HSERickApp/Services/UserDefaultsManager/UDManager.swift
@@ -1,17 +1,41 @@
 import Foundation
 
-class UserDefaultsManager {
+protocol StorageProtocol {
+    func save(character: CharacterModel, to: CharacterCategory)
+    func getCharactersIn(category: CharacterCategory) -> [CharacterModel]
+}
+
+class UserDefaultsManager: StorageProtocol {
+    func save(character: CharacterModel, to category: CharacterCategory) {
+
+        var currentCharacters = getCharactersIn(category: category)
+        if (currentCharacters.filter { $0.id == character.id }).isEmpty {
+            currentCharacters.append(character)
+        }
+
+//        UserDefaults.standard.setValue(currentCharacters, forKey: category.rawValue)
+        UserDefaults.standard.set(try? PropertyListEncoder().encode(currentCharacters), forKey: category.rawValue)
+
+    }
+
+    func getCharactersIn(category: CharacterCategory) -> [CharacterModel] {
+        guard
+            let data = UserDefaults.standard.value(forKey: category.rawValue) as? Data,
+            let charactersArray = try? PropertyListDecoder().decode(Array<CharacterModel>.self, from: data)
+//            let charactersAsAny = UserDefaults.standard.value(forKey: category.rawValue),
+//            let charactersArray = charactersAsAny as? [CharacterModel]
+        else {
+            return [CharacterModel]()
+        }
+        return charactersArray
+    }
+
     private static let instance = UserDefaultsManager()
     class func sharedInstance() -> UserDefaultsManager {
         return instance
     }
 
-    enum DataCategory: String, CaseIterable {
-        case favourites
-        case recent
-    }
-
-    func getCharacterIDsIn(category: DataCategory) -> [UInt] {
+    func getCharacterIDsIn(category: CharacterCategory) -> [UInt] {
         guard
             let characterIDsAsAny = UserDefaults.standard.value(forKey: category.rawValue),
             let characterIDs = characterIDsAsAny as? [UInt]
@@ -21,7 +45,7 @@ class UserDefaultsManager {
         return characterIDs
     }
 
-    func add(id: UInt, to category: DataCategory) {
+    func add(id: UInt, to category: CharacterCategory) {
         var currentIDs = getCharacterIDsIn(category: category)
         if !currentIDs.contains(id) {
             currentIDs.append(id)
@@ -29,14 +53,14 @@ class UserDefaultsManager {
         UserDefaults.standard.setValue(currentIDs, forKey: category.rawValue)
     }
 
-    func remove(id: UInt, from category: DataCategory) {
+    func remove(id: UInt, from category: CharacterCategory) {
         var currentIDs = getCharacterIDsIn(category: category)
         currentIDs = currentIDs.filter {$0 != id}
         UserDefaults.standard.setValue(currentIDs, forKey: category.rawValue)
     }
 
     // Необходимость функции под вопросом
-    func checkIf(id: UInt, in category: DataCategory) -> Bool {
+    func checkIf(id: UInt, in category: CharacterCategory) -> Bool {
         let currentIDs = getCharacterIDsIn(category: category)
         return currentIDs.contains(id)
     }
