a9b385682b9c06291ae64495a1719b92ebe3ba21
Koryakin Alexey <~>
2022-06-04 20:44:50 +0300

    Добавлен мост из ассета цветов в Colors

---
+++ RickAndMortyApp/HSERickApp/Extensions/Color+assetColorAccess.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+extension Color {
+    static let mainTheme = Color.ColorSet()
+
+    struct ColorSet {
+        let mainLabelColor = Color("mainLabelColor")
+        let backgroundColor = Color("backgroundColor")
+        let secondaryLabelColor = Color("secondaryLabelColor")
+        let unselectedFavButton = Color("unselectedFavButton")
+    }
+}
