d5c107936e6da03063b19d6518749ffdf83dce7b
Koryakin Alexey <~>
2022-06-15 01:49:18 +0300

    Добавлен пресет цветов для UIColor

---
+++ RickAndMortyApp/HSERickApp/Extensions/assetColorAccess/UIColor+assetColorAccess.swift.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+extension UIColor {
+    static let mainTheme = UIColor.ColorSet()
+
+    struct ColorSet {
+        let mainLabelColor = Color("mainLabelColor")
+        let backgroundColor = Color("backgroundColor")
+        let secondaryLabelColor = Color("secondaryLabelColor")
+        let unselectedFavButton = Color("unselectedFavButton")
+    }
+}
